e51ed6ea4b8f86ffc34af3c65cf058a4d8e0397e
YashviTala <yashvitala@gmail.com>
2026-01-06 09:18:32 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,14 @@
+x="hello world"
+print(x)
+x="20"
+print(x)
+x="20.5"
+print(x)
+x=frozenset({"apple","banana","cherry"})
+print(x)
+x=True
+print(x)
+x=1j
+print(x)
+x=("apple","banana")
+print(x)
