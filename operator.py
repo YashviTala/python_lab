434aeb7ffd5903ae5faee9cfe545340e54315a74
YashviTala <yashvitala@gmail.com>
2026-01-27 09:24:31 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,40 @@
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+
+x=10 
+print(x)
+
+a=5
+print(-a)
+
+p=10
+q=5
+print(p>q)#true
+print(p==q)#false
+
+x=True
+y=False
+print(x and y)#false
+print(x or y)#true
+print(not x)#false
+
+is_pass=True
+print(is_pass)#true
+
+a=5 #101 in binary
+b=3 #011 in binary
+print(a&b)#1
+print(a|b)#7
+
+
+num=[1,2,3,4]
+print(2 in num)
+print(5 not in num)
+
+x=10
+y=10
+print(x is y)#true
+print(x is not y)#false
