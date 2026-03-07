3712ad596590d2366b88a3645c1dd5aef363ad4f
YashviTala <yashvitala@gmail.com>
2026-03-07 09:08:38 +0530

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,16 @@
+from  array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[0])#first element
+print(arr[2])#third element
+print(arr[4])#fifth element
+
+from  array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])#first element
+print(arr[-2])#third element
+print(arr[-5])#fifth element
+
+from array import array
+arr=array('i',[10,20,30,40,50])
+arr [2] = 35
+print(arr)
