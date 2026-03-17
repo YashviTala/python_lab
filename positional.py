7e995d5acc641eba44056253ee8f5337e7bfe3a6
YashviTala <yashvitala@gmail.com>
2026-03-17 09:37:33 +0530

    Add files via upload

---
+++ positional.py
@@ -0,0 +1,43 @@
+def add(a,b):
+  print ("a=",a)
+  print("b=",b)
+  return a + b
+result = add(2,5)
+print ("sum=",result)
+
+
+def student_info(name,roll,marks):
+    print("name",name)
+    print("roll",roll)
+    print("marks",marks)
+student_info("ravi",101,85)    
+
+def ar_circle(r):
+  a_circle = 3.14*r*r
+  print("area of circle",a_circle)
+  ar_circle(1.5)
+  ar_circle(4)
+
+def check_value (no):
+    if(no>0):
+       print("positive") 
+    elef(no<0):
+       printf("negative")
+    else("zero")
+check_value (0)   
+check_value (45)
+check_value (-67)
+
+def odd_even (no):
+    if(no%2==0):
+        print(f"value {no} is even")
+    else:
+        print(f"value {no} is odd")    
+odd_even (50)
+odd_even (15)
+
+def addition (a,b):
+ add = a + b
+ print("addition of two value",add)
+ addition (50,10,5)
+ addition (100,200)
